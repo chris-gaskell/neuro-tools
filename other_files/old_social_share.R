300bfa1f965f9b26f34c0432d157c96e013a7933
chris-gaskell <chris-gaskell@hotmail.co.uk>
2024-09-15 12:51:46 +0100

    new social-share approach

---
+++ other_files/old_social_share.R
@@ -0,0 +1,16 @@
+<div>
+  
+  ```{=html}
+<a href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+class="twitter-share-button"
+data-text="Learn more about neuropsychological tools using the neuropsytools website!"
+data-via="chrisgaskell92"
+data-hashtags="neuropsychology,neuropsytools"
+data-show-count="true"
+data-size="large"
+data-dnt="true">Tweet</a>
+  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+  
+  ```
+
+</div>
